3ddf433136b8b6c4bf2fa53c0feac4fea4956a1c
krzysztofpozoga <pozoga.krzysztof@gmail.com>
2017-09-21 15:21:59 +0200

    Firebase - added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
 import clean from './clean.js';
+import * as Firebase from 'firebase';
 $(()=>{
+
+  const config = {
+      apiKey: "AIzaSyCO1wIpXqCaxjyZInO1Djg-k0ngVV-sXxo",
+      authDomain: "hisquiz.firebaseapp.com",
+      databaseURL: "https://hisquiz.firebaseio.com",
+      projectId: "hisquiz",
+      storageBucket: "",
+      messagingSenderId: "843638065814"
+    };
+
+  let fb = Firebase.initializeApp(config);
+  let db = fb.database().ref();
+
+  db.on('value', snap => console.log(snap.val()));
+
 
   let questionArray = [];
 
